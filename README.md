3eb8e4f7b81cd8b9aceec623b4ecbdbe97c2fa5d
hokhoa <hokhoa2501@gmail.com>
2020-07-09 22:36:08 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-stsese
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-stsese)
---
+++ src/app/products-alert/products-alert.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-products-alert',
+  templateUrl: './products-alert.component.html',
+  styleUrls: ['./products-alert.component.css']
+})
+export class ProductsAlertComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
